eef1e031eb131cfae9b0b9f9b8bc39869d4df0af
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-11 11:52:33 +0900

    destroyアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_params, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
     @items = Item.order("created_at DESC")
@@ -35,9 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id
-      @item = Item.find(params[:id])
-      @item.destroy
+    if @item.destroy
       redirect_to root_path
     end
   end
