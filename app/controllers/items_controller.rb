eda6b98d37b43916681c857e798c58a441920d87
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-11 11:00:50 +0900

    コントローラー再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,10 +12,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
