f19a0a8cab64a467b01eb5261c01fdcad0e99f60
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-11 11:22:28 +0900

    if文に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
@@ -42,7 +41,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless @item.user_id == current_user.id
+    if @item.user_id != current_user.id
       redirect_to root_path
     end
   end
