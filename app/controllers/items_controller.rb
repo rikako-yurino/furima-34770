013624249291ee981a017ba3da401368d746c2ec
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-11 10:48:53 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, except: [:index, :show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_params, only: [:show, :edit, :update]
   
   def index
     @items = Item.order("created_at DESC")
@@ -15,7 +16,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -24,14 +24,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    end
+
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -46,6 +42,12 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def set_params
+    @item = Item.find(params[:id])
   end
 end
