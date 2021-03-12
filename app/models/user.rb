d0c54e440741b19e5e25d63dd14cf70a789c02fd
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-12 09:07:01 +0900

    モデル・テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  # has_many :orders
+  has_many :orders
 
   with_options presence: true do  
     validates :email, uniqueness: true
